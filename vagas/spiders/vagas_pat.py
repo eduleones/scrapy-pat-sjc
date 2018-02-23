76f4c7fa4502252a6caccee5d97370054c8da157
Eduardo Leones <eduleones@gmail.com>
2018-02-23 09:41:21 -0300

    Update vagas_pat.py

--- vagas/spiders/vagas_pat.py
+++ vagas/spiders/vagas_pat.py
@@ -8,19 +8,12 @@ class VagasPatSpider(scrapy.Spider):
     def parse(self, response):
         tds = response.xpath('.//table//tr')
         for td in tds:
-            #vaga = td.xpath('.//td[@style = "width:245px;"]/text()').extract_first()
             vaga = td.xpath('.//td[contains(@style, "width:245px;")]/text()').extract_first()
-            #local = td.xpath('.//td[@style = "width:70px;"]/text()').extract_first()
             local = td.xpath('.//td[contains(@style, "width:70px;")]/text()').extract_first()
-            #quantidade = td.xpath('.//td[@style = "width:50px;"]/text()').extract_first()
             quantidade = td.xpath('.//td[contains(@style, "width:50px;")]/text()').extract_first()
-            #aceita_pcd = td.xpath('.//td[@style = "width:82px;"]/text()').extract_first()
             aceita_pcd = td.xpath('.//td[contains(@style, "width:82px;")]/text()').extract_first()
-            #exclusiva_pcd = td.xpath('.//td[@style = "width:100px;"]/text()').extract_first()
             exclusiva_pcd = td.xpath('.//td[contains(@style, "width:100px;")]/text()').extract_first()
-            #escolaridade = td.xpath('.//td[@style = "width:175px;"]/text()').extract_first()
             escolaridade = td.xpath('.//td[contains(@style, "width:175px;")]/text()').extract_first()
-            #experiencia = td.xpath('.//td[@style = "width:60px;"]/text()').extract_first()
             experiencia = td.xpath('.//td[contains(@style, "width:60px;")]/text()').extract_first()
             
             yield {
